6ab1ae3f13d5a4899ed503605bebcb2ad7e3a6f8
“mineii” <kmaoo0406@naver.com>
2023-03-16 17:23:20 +0900

    feat: 메인페이지 게시물 카드 레이아웃

--- frontend/src/components/MainPage/Card.tsx
+++ frontend/src/components/MainPage/Card.tsx
@@ -2,7 +2,22 @@ import React from "react";
 
 const Card = () => {
   return <div>
-    
+    <div className="flex flex-col text-center">
+        <div>우리 학교 동창회 서비스</div>
+        <div>moyora</div>
+    </div>
+    <div className="flex">
+        <div>
+            <h1>Title</h1>
+            <p>explain</p>
+            <div>
+            <span>#Spring</span>
+            <span>#React</span>
+            <span>#Typescript</span>
+            </div>
+        </div>
+        <div>123</div>
+    </div>
   </div>;
 };
 
