ce21614df48a4e5b0a276eb66d9506562a59f0de
Mayreeel <kuwon15@gmail.com>
2023-03-19 16:57:44 +0900

    chore : quill 리사이즈 모듈 설치

--- frontend/src/components/QuillEditor.tsx
+++ frontend/src/components/QuillEditor.tsx
@@ -1,6 +1,4 @@
-import React from 'react';
-import { useRef, useState, useMemo } from 'react';
-
+import React, { useRef, useState, useMemo } from 'react';
 import ReactQuill from 'react-quill';
 import 'react-quill/dist/quill.snow.css';
 
@@ -12,48 +10,36 @@ const QuillEditor = () => {
   const imageHandler = () => {
     // 파일을 업로드 하기 위한 input 태그 생성
     const input = document.createElement('input');
-    const formData = new FormData();
-    let url = '';
-
     input.setAttribute('type', 'file');
     input.setAttribute('accept', 'image/*');
     input.click();
 
     // 파일이 input 태그에 담기면 실행 될 함수
-    // input.onchange = async () => {
-    //   const file = input.files;
-    //   if (file !== null) {
-    //     formData.append("image", file[0]);
+    input.onchange = () => {
+      const file = input.files;
+      if (file !== null) {
+        const reader = new FileReader();
 
-    // // 저의 경우 파일 이미지를 서버에 저장했기 때문에
-    // 	// 백엔드 개발자분과 통신을 통해 이미지를 저장하고 불러왔습니다.
-    //     try {
-    //       const res = axios를 통해 백엔드 개발자분과 통신했고, 데이터는 폼데이터로 주고받았습니다.
+        // 파일 읽기가 완료되면 실행 될 함수
+        reader.onload = () => {
+          // 커서의 위치를 알고 해당 위치에 이미지 태그를 넣어주는 코드
+          // 해당 DOM의 데이터가 필요하기에 useRef를 사용
+          const range = QuillRef.current?.getEditor().getSelection()?.index;
+          if (range !== null && range !== undefined) {
+            let quill = QuillRef.current?.getEditor();
 
-    // // 백엔드 개발자 분이 통신 성공시에 보내주는 이미지 url을 변수에 담는다.
-    //       url = res.data.url;
+            quill?.setSelection(range, 1);
 
-    // // 커서의 위치를 알고 해당 위치에 이미지 태그를 넣어주는 코드
-    // 	// 해당 DOM의 데이터가 필요하기에 useRef를 사용한다.
-    //       const range = QuillRef.current?.getEditor().getSelection()?.index;
-    //       if (range !== null && range !== undefined) {
-    //         let quill = QuillRef.current?.getEditor();
+            quill?.clipboard.dangerouslyPasteHTML(
+              range,
+              `<img src=${reader.result} alt="이미지 태그가 삽입됩니다." />`,
+            );
+          }
+        };
 
-    //         quill?.setSelection(range, 1);
-
-    //         quill?.clipboard.dangerouslyPasteHTML(
-    //           range,
-    //           `<img src=${url} alt="이미지 태그가 삽입됩니다." />`
-    //         );
-    //       }
-
-    //       return { ...res, success: true };
-    //     } catch (error) {
-    //       const err = error as AxiosError;
-    //       return { ...err.response, success: false };
-    //     }
-    //   }
-    // };
+        reader.readAsDataURL(file[0]);
+      }
+    };
   };
 
   const modules = useMemo(
