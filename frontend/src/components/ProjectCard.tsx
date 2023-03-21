6dc499d894ec5740f02ceeaae73a8bc500f34457
“mineii” <kmaoo0406@naver.com>
2023-03-22 02:49:38 +0900

    design: 이미지 추가 및 css 수정

--- frontend/src/components/ProjectCard.tsx
+++ frontend/src/components/ProjectCard.tsx
@@ -1,17 +1,23 @@
 import React from 'react';
+import project from '../assets/image/project.png';
 
 const ProjectCard = () => {
   return (
     <div className="rounded-md drop-shadow-md">
-      <div className="flex flex-col text-center w-187 h-150 bg-rose-100 rounded-t-lg">
-        <div>우리 학교 동창회 서비스</div>
-        <div>moyora</div>
+      <div className="flex flex-col justify-center text-center w-[190px] h-[150px] bg-sky-100 rounded-t-lg">
+        {/* <div>우리 학교 동창회 서비스</div>
+        <div>moyora</div> */}
+        <img
+          className="flex flex-col justify-center text-center w-[190px] h-[150px] bg-sky-100 rounded-t-lg"
+          src={project}
+          alt="프로젝트 이미지"
+        />
       </div>
 
-      <div className="flex w-187 h-76 bg-stone-50 rounded-b-lg">
+      <div className="flex justify-center w-[190px] h-[83px] bg-stone-50 rounded-b-lg">
         <div className="grow">
-          <h1>Title</h1>
-          <p>explain</p>
+          <h1 className="font-bold text-lg">Title</h1>
+          <p className="font-semibold">explain</p>
           <div>
             <span>#Spring</span>
             <span>#React</span>
--- frontend/src/pages/MainPage.tsx
+++ frontend/src/pages/MainPage.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import TopBar from '../components/TopBar';
+// import TopBar from '../components/TopBar';
 import Banner from '../components/Banner';
 import ProjectCard from '../components/ProjectCard';
 
@@ -9,23 +9,25 @@ const MainPage = () => {
     <div>
       {/* <TopBar/> */}
       <Banner />
-      <div className="justify-center content-center grid grid-cols-4 gap-10 mb-8">
-        <ProjectCard />
-        <ProjectCard />
-        <ProjectCard />
-        <ProjectCard />
-        <ProjectCard />
-        <ProjectCard />
-        <ProjectCard />
-        <ProjectCard />
-        <ProjectCard />
-        <ProjectCard />
-        <ProjectCard />
-        <ProjectCard />
-        <ProjectCard />
-        <ProjectCard />
-        <ProjectCard />
-        <ProjectCard />
+      <div className="justify-center content-center">
+        <div className="w-full grid grid-cols-4 gap-10">
+          <ProjectCard />
+          <ProjectCard />
+          <ProjectCard />
+          <ProjectCard />
+          <ProjectCard />
+          <ProjectCard />
+          <ProjectCard />
+          <ProjectCard />
+          <ProjectCard />
+          <ProjectCard />
+          <ProjectCard />
+          <ProjectCard />
+          <ProjectCard />
+          <ProjectCard />
+          <ProjectCard />
+          <ProjectCard />
+        </div>
       </div>
     </div>
   );
