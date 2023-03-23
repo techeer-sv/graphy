83ef4b99cf80968de88cbc632d06f7893f7721ac
Mayreeel <kuwon15@gmail.com>
2023-03-23 18:55:15 +0900

    feat : 버튼 추가

--- frontend/src/pages/ReadingPage.tsx
+++ frontend/src/pages/ReadingPage.tsx
@@ -18,10 +18,26 @@ function ReadingPage() {
         <div></div>
         {/**버튼 영역**/}
         <div>
-          {/**수정 버튼**/}
-          <div></div>
-          {/**삭제 버튼**/}
-          <div></div>
+          <div className="mt-20 mb-4 flex justify-end sm:mt-20 lg:mt-12">
+            <button
+              className="focus:shadow-outline mr-2 h-12 w-24 appearance-none rounded-sm border bg-gray-500 font-ng text-white hover:bg-gray-700"
+              onClick={() => console.log('수정 버튼 클릭')}
+            >
+              수정
+            </button>
+            <button
+              className="focus:shadow-outline mr-2 h-12 w-24 appearance-none rounded-sm border bg-gray-500 font-ng text-white hover:bg-gray-700"
+              onClick={() => console.log('삭제 버튼 클릭')}
+            >
+              삭제
+            </button>
+            <button
+              className="focus:shadow-outline h-12 w-24 appearance-none rounded-sm bg-blue-500 font-ng text-white hover:bg-blue-700"
+              onClick={() => console.log('글작성 버튼 클릭')}
+            >
+              글작성
+            </button>
+          </div>
         </div>
       </div>
     </div>
