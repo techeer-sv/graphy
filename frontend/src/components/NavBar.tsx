04b93cd907f76be53ebeb56daf0f7a0da76e0594
“mineii” <kmaoo0406@naver.com>
2023-03-25 02:33:44 +0900

    feat: 프로젝트 공유 버튼 반응형 위치

--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -16,16 +16,16 @@ const NavBar = () => {
       <input
         type="text"
         placeholder=" search"
-        className="w-[900px] h-[40px] mx-5 ml-15"
+        className="w-[900px] h-[40px] mx-5 rounded-xl"
       />
 
       {/* 프로젝트 작성 버튼 */}
-      <button className="ml-5 sm:ml-10 rounded-full bg-graphyblue text-slate-50 font-semibold px-4 py-1 shrink-0">
+      <button className="hidden ml-1 sm:ml-1 sm:block rounded-full bg-graphyblue text-slate-50 font-semibold px-4 py-1 shrink-0">
         프로젝트 공유
       </button>
 
       {/* 마이페이지 아이콘 */}
-      <button className="w-[50px] h-[30px] mx-auto ml-7 mr-10">
+      <button className="w-[50px] h-[30px] mr-10">
         <FaceIcon />
       </button>
     </div>
--- frontend/src/pages/MainPage.tsx
+++ frontend/src/pages/MainPage.tsx
@@ -6,11 +6,18 @@ import ProjectCard from '../components/ProjectCard';
 
 const MainPage = () => {
   return (
-    <div className="bg-gray-50 w-screen">
+    <div className="bg-gray-50 w-screen h-screen relative">
       <NavBar />
-      <Banner />
-      <div className="text-2xl font-medium ml-10 mb-5">All</div>
-      <div className="justify-center content-center">
+      <div>
+        <Banner />
+        <button
+          className="fixed bottom-10 right-10 z-10 sm:hidden shrink-0 my-auto mb-2
+          rounded-full bg-graphyblue text-slate-50 font-semibold px-4 py-1
+          "
+        >
+          프로젝트 공유
+        </button>
+        <div className="text-2xl font-medium ml-10 mb-5">All</div>
         <div className="w-11/12 flex flex-wrap justify-center gap-10 ml-8">
           <ProjectCard />
           <ProjectCard />
