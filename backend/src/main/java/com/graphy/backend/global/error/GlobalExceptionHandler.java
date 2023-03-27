e139eee7208c3ddab07c3ae02612a7bf2606cae8
halin <halin130@naver.com>
2023-03-27 23:41:28 +0900

    chore: ErrorResponse 세팅

---
+++ backend/src/main/java/com/graphy/backend/global/error/GlobalExceptionHandler.java
@@ -0,0 +1,62 @@
+package com.graphy.backend.global.error;
+
+import com.graphy.backend.global.error.exception.BusinessException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import static org.springframework.http.HttpStatus.BAD_REQUEST;
+
+@Slf4j
+@RestControllerAdvice
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    protected ResponseEntity<ErrorResponse> handleException(Exception e) {
+        log.error(e.getMessage(), e);
+        ErrorResponse response = ErrorResponse.of(ErrorCode.INTERNAL_SERVER_ERROR);
+        return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler
+    protected ResponseEntity<ErrorResponse> handleRuntimeException(BusinessException e) {
+        final ErrorCode errorCode = e.getErrorCode();
+        final ErrorResponse response = makeErrorResponse(errorCode);
+        log.warn(e.getMessage());
+        return new ResponseEntity<>(response, errorCode.getStatus());
+    }
+
+    @ExceptionHandler
+    protected ResponseEntity<ErrorResponse> handleMethodArgumentNotValidException(
+            MethodArgumentNotValidException e) {
+        final ErrorResponse response =
+                ErrorResponse.of(ErrorCode.INPUT_INVALID_VALUE, e.getBindingResult());
+        log.warn(e.getMessage());
+        return new ResponseEntity<>(response, BAD_REQUEST);
+    }
+
+    private ResponseEntity<Object> handleExceptionInternal(
+            Exception e, ErrorCode errorCode, WebRequest request) {
+        log.error(e.getMessage(), e);
+        return handleExceptionInternal(e, errorCode, errorCode.getStatus(), request);
+    }
+
+    private ResponseEntity<Object> handleExceptionInternal(
+            Exception e, ErrorCode errorCode, HttpStatus status, WebRequest request) {
+        return super.handleExceptionInternal(
+                e, ErrorResponse.of(errorCode), HttpHeaders.EMPTY, status, request);
+    }
+
+    private ErrorResponse makeErrorResponse(ErrorCode errorCode) {
+        return ErrorResponse.builder()
+                        .message(errorCode.getMessage())
+                        .code(errorCode.getErrorCode())
+                        .build();
+    }
+}
