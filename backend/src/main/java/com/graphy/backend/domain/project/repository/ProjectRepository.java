0a2841be7213125d5f8730d5b2d8f3e181dfdaa9
halin <halin130@naver.com>
2023-04-03 14:18:10 +0900

    refactor: Project Search API 리팩토링

--- backend/src/main/java/com/graphy/backend/domain/project/repository/ProjectRepository.java
+++ backend/src/main/java/com/graphy/backend/domain/project/repository/ProjectRepository.java
@@ -3,10 +3,15 @@ package com.graphy.backend.domain.project.repository;
 import com.graphy.backend.domain.project.domain.Project;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.stereotype.Repository;
 
 
-public interface ProjectRepository extends JpaRepository<Project, Long> {
-    Page<Project> findByProjectNameContaining(String project_name, Pageable pageable);
-    Page<Project> findByContentContaining(String content, Pageable pageable);
+@Repository
+public interface ProjectRepository extends JpaRepository<Project, Long>, JpaSpecificationExecutor<Project> {
+    Page<Project> findAll(Specification<Project> spec, Pageable pageable);
+
+
 }
