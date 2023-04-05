1845e527704ff8cda230790c82aef736e4714511
Mayreeel <kuwon15@gmail.com>
2023-04-05 23:17:17 +0900

    feat: 새로고침 했을때도 정보 유지

--- frontend/src/Recoil.tsx
+++ frontend/src/Recoil.tsx
@@ -1,20 +1,27 @@
 import { atom } from 'recoil';
+import { recoilPersist } from 'recoil-persist';
+
+const { persistAtom } = recoilPersist();
 
 const titleState = atom<string>({
   key: 'titleState',
   default: '',
+  effects_UNSTABLE: [persistAtom],
 });
 const tldrState = atom<string>({
   key: 'tldrState',
   default: '',
+  effects_UNSTABLE: [persistAtom],
 });
 const selectedStackState = atom<string[]>({
   key: 'selectedStackState',
   default: [],
+  effects_UNSTABLE: [persistAtom],
 });
 const contentsState = atom({
   key: 'contentsState',
   default: '',
+  effects_UNSTABLE: [persistAtom],
 });
 const thumbnailUrlState = atom<string | null>({
   key: 'thumbnailUrlState',
@@ -23,6 +30,7 @@ const thumbnailUrlState = atom<string | null>({
 const projectIdState = atom<number>({
   key: 'projectIdState',
   default: 0,
+  effects_UNSTABLE: [persistAtom],
 });
 
 export {
