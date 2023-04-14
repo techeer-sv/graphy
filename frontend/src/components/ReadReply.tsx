df1b6a2120d1637fdb8fb49458e8643e71d7272f
Mayreeel <kuwon15@gmail.com>
2023-04-15 01:10:21 +0900

    feat : 대댓글 입력 열고 닫기

--- frontend/src/components/ReadReply.tsx
+++ frontend/src/components/ReadReply.tsx
@@ -1,14 +1,21 @@
+import { useState } from 'react';
+
 import nested_reply from '../assets/image/nested_reply.svg';
 import ReadReReply from './ReadReReply';
 import WriteReReply from './WriteReReply';
 
 function ReadReply(props: any) {
+  const [visible, setVisible] = useState<boolean>(false);
+
   return (
     <>
       <div className="mt-3 h-auto rounded-lg border-2 border-gray-400">
         <div className=" flex flex-row border-b border-dashed border-gray-400 py-1 pl-2 font-ng text-sm">
-          {`댓글 id = ${props.contents.commentId}`}
-          <button className="mx-auto mr-0 flex border-l border-dashed border-gray-400 pr-2 pl-2">
+          {`댓글 Id = ${props.contents.commentId}`}
+          <button
+            className="mx-auto mr-0 flex border-l border-dashed border-gray-400 pr-2 pl-2"
+            onClick={() => setVisible(!visible)}
+          >
             <img src={nested_reply} className="mt-1 mr-1 h-3 font-ng text-sm" />
             답글
           </button>
@@ -22,7 +29,7 @@ function ReadReply(props: any) {
         <ReadReReply contents={x} key={y} />
       ))}
       {/*대댓글 입력창*/}
-      <WriteReReply />
+      {visible ? <WriteReReply /> : null}
     </>
   );
 }
