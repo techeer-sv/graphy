ca35546776dc97803bbf4e93ea513b471af03c9c
“mineii” <kmaoo0406@naver.com>
2023-04-15 15:51:33 +0900

    feat: 프로젝트 카드 페이지 연결

--- frontend/src/components/ProjectCard.tsx
+++ frontend/src/components/ProjectCard.tsx
@@ -1,18 +1,30 @@
 import React from 'react';
 import project from '../assets/image/project.png';
 import AllStacks from '../Stack'
+import { useNavigate } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import { useRecoilState } from 'recoil';
+import { projectIdState } from '../Recoil';
+
 
 function ProjectCard(items:any) {
   console.log(items.items.projectName);
 
+  const [projectId, setProjectId] = useRecoilState(projectIdState);
+  
 function findImage(tag:string) {
   //AllStacks.map(x => x.name).findIndex(x => x == tag)
   //AllStacks.map(x => x.image)[AllStacks.map(x => x.name).findIndex(x => x == tag)]
   return AllStacks.map(x => x.image)[AllStacks.map(x => x.name).findIndex(x => x == tag)];
 }
+const navigate = useNavigate();
 
+function toRead() {
+  navigate('/read');
+  setProjectId(items.items.projectId);
+}
   return (
-    <div className="rounded-md drop-shadow-md">
+    <button onClick={() => toRead()} className="rounded-md drop-shadow-md">
       <div className="flex h-[160px] w-[200px] flex-col justify-center rounded-t-lg bg-sky-100 text-center">
         {/* <div>우리 학교 동창회 서비스</div>
         <div>moyora</div> */}
@@ -32,7 +44,7 @@ function findImage(tag:string) {
           </div>
         </div>
       </div>
-    </div>
+    </button>
   );
 };
 
