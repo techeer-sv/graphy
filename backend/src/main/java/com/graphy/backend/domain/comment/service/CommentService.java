30168e6e689737f71f9f711c26089abbd7be42ce
dbrjs4594283 <dbrjs4594283@naver.com>
2023-04-16 20:31:53 +0900

    feat : 댓글 삭제 API 구현

--- backend/src/main/java/com/graphy/backend/domain/comment/service/CommentService.java
+++ backend/src/main/java/com/graphy/backend/domain/comment/service/CommentService.java
@@ -9,6 +9,8 @@ import com.graphy.backend.global.error.exception.EmptyResultException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
+
 import static com.graphy.backend.domain.comment.dto.CommentDto.CreateCommentRequest;
 import static com.graphy.backend.domain.comment.dto.CommentDto.CreateCommentResponse;
 
@@ -34,5 +36,36 @@ public class CommentService {
         Comment entity = CreateCommentRequest.to(dto, project, parentComment);
 
         return new CreateCommentResponse(commentRepository.save(entity).getId());
+    }
+
+    @Transactional
+    public void deleteComment(Long id) {
+        Comment comment = commentRepository.findById(id)
+                .orElseThrow(() -> new EmptyResultException(ErrorCode.PROJECT_DELETED_OR_NOT_EXIST));
+
+        // 대댓글인 경우
+        if (comment.getParent() != null) {
+            // 삭제
+            comment.setDeletedTrue();
+            //commentRepository.delete(comment);
+
+            //부모 댓글이 삭제된 상태고 다른 대댓글이 없으면 부모 댓글도 삭제
+            if (comment.getParent().getContent().equals("삭제된 댓글입니다.") &&
+                    comment.getParent().getChildList().size() == 1) {
+                comment.getParent().setDeletedTrue();
+//                commentRepository.delete(comment.getParent());
+            }
+
+            // 부모 댓글인 경우
+        } else {
+
+            // 자식 댓글이 없으면 삭제
+            if (comment.getChildList().isEmpty()) {
+                comment.setDeletedTrue();
+//                commentRepository.delete(comment);
+            }
+            // 자식 댓글이 남았으면 "삭제된 댓글입니다."
+            comment.deleteComment(comment);
+        }
     }
 }
