8d82c626c0e35d9a00b417f9beecf146d3fc1181
“mineii” <kmaoo0406@naver.com>
2023-04-23 22:18:01 +0900

    feat: 서브배너

--- frontend/src/components/Banner.tsx
+++ frontend/src/components/Banner.tsx
@@ -2,6 +2,8 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
 import mainbanner from '../assets/image/mainbanner.svg';
 import subbanner from '../assets/image/subbanner.svg'
+import Lottie from 'lottie-react';
+import { lottie } from '../assets/lottie';
 
 function Banner() {
 
@@ -17,7 +19,12 @@ function Banner() {
         <p className='mt-16 ml-20 font-lef-b text-4xl text-white border-b-4 pb-3 pr-0 w-fit '>프로젝트 기록과 공유의 공간, Graphy</p>
         <p className='mt-3 ml-20 font-lef text-2xl text-white'>Graphy와 함께 성장해보세요.</p>
       </SwiperSlide>
-      <SwiperSlide className='bg-subbanner'></SwiperSlide>
+      <SwiperSlide className='bg-subbanner flex'>
+        <div><p className='mt-16 ml-20 font-lef-b text-4xl text-zinc-800 '>프로젝트 공유하러 출발!</p>
+        <p className='mt-6 ml-20 font-lef text-2xl text-stone-800'>Graphy에서 누구나 쉽게 프로젝트를 작성하고 공유할 수 있습니다. 이제 출발하세요!</p>
+        </div>
+        <Lottie className='sm:w-52 sm:h-52 h-0 w-0 bg-subbanner mt-8 mr-28 mx-auto' animationData={lottie} />
+      </SwiperSlide>
     </Swiper>
   </div>;
 }
