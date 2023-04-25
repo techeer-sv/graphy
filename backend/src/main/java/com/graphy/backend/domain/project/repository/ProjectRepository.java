4b3f2c9ce534ff0b5243edeeeaa3b15a453d2ad8
halin <halin130@naver.com>
2023-04-25 14:15:55 +0900

    chore: 사용하지 않는 Specification 코드 삭제

--- backend/src/main/java/com/graphy/backend/domain/project/repository/ProjectRepository.java
+++ backend/src/main/java/com/graphy/backend/domain/project/repository/ProjectRepository.java
@@ -3,14 +3,13 @@ package com.graphy.backend.domain.project.repository;
 import com.graphy.backend.domain.project.domain.Project;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 
 
 @Repository
-public interface ProjectRepository extends JpaRepository<Project, Long>, JpaSpecificationExecutor<Project> {
-    Page<Project> findAll(Specification<Project> spec, Pageable pageable);
+public interface ProjectRepository extends JpaRepository<Project, Long>, ProjectCustomRepository {
+    Page<Project> searchProjectsWith(Pageable pageable, String projectName, String content);
+
 }
