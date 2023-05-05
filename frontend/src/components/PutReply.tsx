5870ebb00d1704cdc719a08e7ac2d7afeb351109
Mayreeel <kuwon15@gmail.com>
2023-05-06 04:51:54 +0900

    fix : props.contents 속성 확인 오류 수정

--- frontend/src/components/PutReply.tsx
+++ frontend/src/components/PutReply.tsx
@@ -42,7 +42,9 @@ function PutReply(props: any) {
       const res = await axios.put(url, data);
       console.log(res.data);
       setrefresh(!refresh);
-      props.hasOwnProperty('childCount') ? null : props.changeCommentRef();
+      props.contents.hasOwnProperty('childCount')
+        ? null
+        : props.changeCommentRef();
       setValue('');
     } catch (error) {
       console.error(error);
