23e3e17c9dc5ca215898aac4a8898361467064bf
“mineii” <kmaoo0406@naver.com>
2023-05-11 22:07:34 +0900

    feat: 기술 선택

--- frontend/src/components/Modal.tsx
+++ frontend/src/components/Modal.tsx
@@ -5,6 +5,50 @@ import monster from '../assets/image/monster.png';
 import desktop from '../assets/image/desktop.png';
 import pick from '../assets/image/pick.png';
 import eyes from '../assets/image/eyes.png';
+import Android_studio from '../assets/image/stacklogo/Android_studio.svg';
+import Apache_cassandra from '../assets/image/stacklogo/Apache_cassandra.svg';
+import Apache_kafka from '../assets/image/stacklogo/Apache_kafka.svg';
+import Apache_spark from '../assets/image/stacklogo/Apache_spark.svg';
+import Aws from '../assets/image/stacklogo/AWS.svg';
+import Clang from '../assets/image/stacklogo/Clang.svg';
+import Cpp from '../assets/image/stacklogo/Cpp.svg';
+import CSharp from '../assets/image/stacklogo/CSharp.svg';
+import Django from '../assets/image/stacklogo/Django.svg';
+import Docker from '../assets/image/stacklogo/Docker.svg';
+import Express from '../assets/image/stacklogo/Express.svg';
+import Fastapi from '../assets/image/stacklogo/Fastapi.svg';
+import Firebase from '../assets/image/stacklogo/Firebase.svg';
+import Flask from '../assets/image/stacklogo/Flask.svg';
+import Flutter from '../assets/image/stacklogo/Flutter.svg';
+import Github_actions from '../assets/image/stacklogo/Github_actions.svg';
+import Go from '../assets/image/stacklogo/Go.svg';
+import GraphQL from '../assets/image/stacklogo/GraphQL.svg';
+import Java from '../assets/image/stacklogo/Java.svg';
+import JavaScript from '../assets/image/stacklogo/JavaScript.svg';
+import Jenkins from '../assets/image/stacklogo/Jenkins.svg';
+import Julia from '../assets/image/stacklogo/Julia.svg';
+import Kotlin from '../assets/image/stacklogo/Kotlin.svg';
+import Kubernetes from '../assets/image/stacklogo/Kubernetes.svg';
+import Mongodb from '../assets/image/stacklogo/Mongodb.svg';
+import Mysql from '../assets/image/stacklogo/Mysql.svg';
+import Nextjs from '../assets/image/stacklogo/Nextjs.svg';
+import Nodejs from '../assets/image/stacklogo/Nodejs.svg';
+import Nestjs from '../assets/image/stacklogo/Nestjs.svg';
+import Php from '../assets/image/stacklogo/Php.svg';
+import Python from '../assets/image/stacklogo/Python.svg';
+import PyTorch from '../assets/image/stacklogo/PyTorch.svg';
+import react from '../assets/image/stacklogo/React.svg';
+import React_native from '../assets/image/stacklogo/React_native.svg';
+import Redux from '../assets/image/stacklogo/Redux.svg';
+import Redis from '../assets/image/stacklogo/Redis.svg';
+import Rust from '../assets/image/stacklogo/Rust.svg';
+import Spring from '../assets/image/stacklogo/Spring.svg';
+import Svelte from '../assets/image/stacklogo/Svelte.svg';
+import Swift from '../assets/image/stacklogo/Swift.svg';
+import TensorFlow from '../assets/image/stacklogo/TensorFlow.svg';
+import Typescript from '../assets/image/stacklogo/Typescript.svg';
+import Unity from '../assets/image/stacklogo/Unity.svg';
+import Vue from '../assets/image/stacklogo/Vue.svg';
 
 //onClickToggleModal이라는 함수 타입을 정의한 인터페이스
 interface ModalDefaultType {
@@ -19,7 +63,7 @@ function Modal({
     <>
       {/* 실제 모달을 나타내는 컨테이너 */}
       <div
-        className="-translate-y-1/2-translate-y-1/2 fixed top-20 right-1/2 z-50 box-border h-[550px]
+        className="-translate-y-1/2-translate-y-1/2 fixed top-20 right-1/2 z-50 box-border h-[620px]
         w-[720px] translate-x-1/2 transform 
         rounded-[30px] bg-white"
       >
@@ -68,12 +112,32 @@ function Modal({
           </div>
 
           {/* 기술 선택 버튼 */}
-          {/* <div className="mt-8 flex justify-center">
-            <button className="flex items-center rounded-full border-[1.5px] border-zinc-300 px-3 py-1">
+          <div className="mt-8 flex justify-center">
+            <button className="mr-2 flex items-center rounded-full border-[1.5px] border-zinc-300 px-3 py-1 text-gray-500">
               <img className="mr-1 h-[20px] w-[20px]" src={react} />
               React
             </button>
-          </div> */}
+            <button className="mr-2 flex items-center rounded-full border-[1.5px] border-zinc-300 px-3 py-1 text-gray-500">
+              <img className="mr-1 h-[20px] w-[20px]" src={Spring} />
+              Spring
+            </button>
+            <button className="mr-2 flex items-center rounded-full border-[1.5px] border-zinc-300 px-3 py-1 text-gray-500">
+              <img className="mr-1 h-[20px] w-[20px]" src={Redux} />
+              Redux
+            </button>
+            {/* <button className="flex items-center rounded-full border-[1.5px] border-zinc-300 px-3 py-1 text-gray-500">
+              <img className="mr-1 h-[20px] w-[20px]" src={Android_studio} />
+              AndroidStudio
+            </button> */}
+            <button className="mr-2 flex items-center rounded-full border-[1.5px] border-zinc-300 px-3 py-1 text-gray-500">
+              <img className="mr-1 h-[20px] w-[20px]" src={Firebase} />
+              Firebase
+            </button>
+            <button className="flex items-center rounded-full border-[1.5px] border-zinc-300 px-3 py-1 text-gray-500">
+              <img className="mr-1 h-[20px] w-[20px]" src={Aws} />
+              Aws
+            </button>
+          </div>
 
           {/* 하단 버튼 */}
           <button
