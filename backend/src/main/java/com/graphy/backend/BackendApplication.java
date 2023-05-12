b8295cc320600dcc898d489e836a2c6fe83c87b5
dbrjs4594283 <dbrjs4594283@naver.com>
2023-05-13 00:09:43 +0900

    refactor:AopConfig 수정

--- backend/src/main/java/com/graphy/backend/BackendApplication.java
+++ backend/src/main/java/com/graphy/backend/BackendApplication.java
@@ -6,7 +6,6 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication
-@EnableAspectJAutoProxy
 public class BackendApplication {
 
     public static void main(String[] args) {
