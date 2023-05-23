3807aecc0beefdc8f0d1264ee4647e76e3e09f15
Mayreeel <kuwon15@gmail.com>
2023-05-23 15:43:33 +0900

    feat : QuillWritten 테스트

---
+++ frontend/src/test/components/QuillWritten.test.tsx
@@ -0,0 +1,33 @@
+import '@testing-library/jest-dom';
+import { render, screen } from '@testing-library/react';
+import { MutableSnapshot, RecoilRoot } from 'recoil';
+import { contentsState } from '../../Recoil';
+import QuillWritten from '../../components/QuillWritten';
+import { onChange, RecoilObserver } from '../jest/RecoilObserver';
+
+describe('QuillWritten', () => {
+  test('noContents 테스트', async () => {
+    render(
+      <RecoilRoot>
+        <RecoilObserver node={contentsState} onChange={onChange} />
+        <QuillWritten />
+      </RecoilRoot>,
+    );
+    const noContents = screen.getByText('게시글을 불러오는 중입니다...');
+    expect(noContents).toBeInTheDocument();
+  });
+  test('Contents 테스트', async () => {
+    render(
+      <RecoilRoot
+        initializeState={(snapshot: MutableSnapshot) => {
+          snapshot.set(contentsState, '테스트 데이터');
+        }}
+      >
+        <RecoilObserver node={contentsState} onChange={onChange} />
+        <QuillWritten />
+      </RecoilRoot>,
+    );
+    const Contents = screen.getByText('테스트 데이터');
+    expect(Contents).toBeInTheDocument();
+  });
+});
