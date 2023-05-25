e070f24b61f19ab797f1570b0544d7a614c39a06
“mineii” <kmaoo0406@naver.com>
2023-05-25 21:13:32 +0900

    feat: 모든 창 반응형

--- frontend/src/components/RenderModal.tsx
+++ frontend/src/components/RenderModal.tsx
@@ -23,7 +23,7 @@ const Screen1: React.FC<Screen1Props> = ({ onNext }) => {
   return (
     <div
       className="-translate-y-1/2-translate-y-1/2 fixed top-20 right-1/2 z-50 mt-5 box-border 
-        h-660 w-[400px] translate-x-1/2 transform rounded-[30px] 
+        h-660 w-[410px] translate-x-1/2 transform rounded-[30px] 
         bg-white sm:w-630"
     >
       {/* 이전/다음 */}
@@ -37,7 +37,7 @@ const Screen1: React.FC<Screen1Props> = ({ onNext }) => {
       </button>
 
       {/* 1단계 */}
-      <ol className="ml-14 mt-4 mb-8 flex w-80 items-center justify-center sm:ml-44">
+      <ol className="ml-16 mt-4 mb-8 flex w-80 items-center justify-center sm:ml-44">
         <li className="flex w-full items-center text-blue-600 after:inline-block after:h-1 after:w-full after:border-4 after:border-b after:border-blue-100 after:content-[''] dark:text-blue-500 dark:after:border-blue-800">
           <span className="flex h-10 w-10 shrink-0 items-center justify-center rounded-full bg-blue-100 dark:bg-blue-800 lg:h-12 lg:w-12">
             <img className="h-[20px] w-[20px] " src={monster} />
@@ -68,7 +68,7 @@ const Screen1: React.FC<Screen1Props> = ({ onNext }) => {
       </div>
       {/* 기술 선택 버튼 */}
       {/* 배열에 요소를 처음부터 하나씩 읽는 게 map, 하나씩 읽을 때 요소를 가리키는 게 x, y는 요소의 인덱스 */}
-      <div className="mt-8 flex h-80 w-[400px] flex-wrap justify-center overflow-auto sm:w-630 ">
+      <div className="mt-8 flex h-355 w-[400px] flex-wrap justify-center overflow-auto sm:w-630 ">
         {AllStacks.map((x, y) => (
           <button
             key={y}
@@ -82,9 +82,9 @@ const Screen1: React.FC<Screen1Props> = ({ onNext }) => {
 
       {/* 하단 버튼 */}
       <button
-        className="ml-16  flex items-center
-            justify-center rounded-[16px]
-          bg-gptbutton px-60 py-1 pt-3 pb-3 font-lef-b text-slate-50 hover:bg-button"
+        className="ml-7 mt-7 flex items-center justify-center
+            rounded-[16px] bg-gptbutton
+          px-40 py-1 pt-3 pb-3 font-lef-b text-slate-50 hover:bg-button sm:ml-504 sm:px-60"
         onClick={onNext}
       >
         다음
@@ -102,31 +102,29 @@ const Screen2: React.FC<Screen2Props> = ({ onPrev, onNext }) => {
   return (
     <div
       className="-translate-y-1/2-translate-y-1/2 fixed top-20 right-1/2 z-50 box-border h-660
-        w-630 translate-x-1/2 transform 
-        rounded-[30px] bg-white"
+        w-[410px] translate-x-1/2  transform rounded-[30px] 
+        bg-white sm:w-630"
     >
       {/* 이전/다음 */}
-      <div className="mt-5">
-        <div className="ml-5 flex font-lef text-gray-400 ">
-          <button
-            onClick={onPrev}
-            className="flex w-56 items-center  hover:text-gray-600"
-          >
-            <img className="mr-2" src={arrowLeftIcon} />
-            이전
-          </button>
+      <div className="">
+        <button
+          onClick={onPrev}
+          className="mx-auto mt-5 ml-5 flex items-center font-lef text-gray-400 hover:text-gray-600"
+        >
+          <img className="mr-2" src={arrowLeftIcon} />
+          이전
+        </button>
 
-          <button
-            onClick={onNext}
-            className="ml-460 flex w-56 items-center  hover:text-gray-600"
-          >
-            <div>다음</div>
-            <img className="ml-2" src={arrowRightIcon} />
-          </button>
-        </div>
+        <button
+          onClick={onNext}
+          className="mx-auto mt-5 mr-5 flex  items-center font-lef text-gray-400 hover:text-gray-600"
+        >
+          <div>다음</div>
+          <img className="ml-2" src={arrowRightIcon} />
+        </button>
 
         {/* 2단계 */}
-        <ol className="ml-44 mt-4 mb-8 flex w-80 items-center justify-center">
+        <ol className=" ml-16  mt-4 mb-8 flex w-80 items-center justify-center sm:ml-44">
           <li className="flex w-full items-center text-blue-600 after:inline-block after:h-1 after:w-full after:border-4 after:border-b after:border-blue-100 after:content-[''] dark:text-blue-500 dark:after:border-blue-800">
             <span className="flex h-10 w-10 shrink-0 items-center justify-center rounded-full bg-blue-100 dark:bg-blue-800 lg:h-12 lg:w-12">
               <img className="h-[20px] w-[20px] " src={monster} />
@@ -160,16 +158,16 @@ const Screen2: React.FC<Screen2Props> = ({ onPrev, onNext }) => {
           <input
             type="text"
             maxLength={20}
-            className="ml-[60px] flex w-10/12 justify-center border-b-2 focus:outline-none"
+            className="ml-9 flex w-10/12 justify-center border-b-2 focus:outline-none sm:ml-[60px]"
             placeholder=" 예시 - 팀원 모집 웹 사이트"
           />
         </div>
         {/* 하단 버튼 */}
         <button
           onClick={onNext}
-          className="fixed ml-16 mt-80
-          flex items-center justify-center
-        rounded-[16px] bg-gptbutton px-60 py-1 pt-3 pb-3 font-lef-b text-slate-50  hover:bg-button"
+          className="fixed  mt-80
+          ml-7 flex items-center justify-center rounded-[16px]  bg-gptbutton px-40
+        py-1 pt-3 pb-3 font-lef-b text-slate-50 hover:bg-button sm:ml-504  sm:px-60"
         >
           다음
         </button>
@@ -199,31 +197,29 @@ const Screen3: React.FC<Screen3Props> = ({ onPrev, onNext }) => {
   return (
     <div
       className="-translate-y-1/2-translate-y-1/2 fixed top-20 right-1/2 z-50 box-border h-660
-        w-630 translate-x-1/2 transform 
-        rounded-[30px] bg-white"
+        w-[410px] translate-x-1/2 transform rounded-[30px] 
+        bg-white sm:w-630"
     >
       {/* 이전/다음 */}
-      <div className="mt-5">
-        <div className="ml-5 flex font-lef text-gray-400 ">
-          <button
-            onClick={onPrev}
-            className="flex w-56 items-center  hover:text-gray-600"
-          >
-            <img className="mr-2" src={arrowLeftIcon} />
-            이전
-          </button>
+      <div className="">
+        <button
+          onClick={onPrev}
+          className="mx-auto mt-5 ml-5 flex  items-center font-lef text-gray-400 hover:text-gray-600"
+        >
+          <img className="mr-2" src={arrowLeftIcon} />
+          이전
+        </button>
 
-          <button
-            onClick={onNext}
-            className="ml-460 flex w-56 items-center  hover:text-gray-600"
-          >
-            <div>다음</div>
-            <img className="ml-2" src={arrowRightIcon} />
-          </button>
-        </div>
+        <button
+          onClick={onNext}
+          className="mx-auto mt-5 mr-5 flex  items-center font-lef text-gray-400 hover:text-gray-600"
+        >
+          <div>다음</div>
+          <img className="ml-2" src={arrowRightIcon} />
+        </button>
 
         {/* 3단계 */}
-        <ol className="ml-44 mt-4 mb-8 flex w-80 items-center justify-center">
+        <ol className="ml-16  mt-4  mb-8 flex w-80 items-center justify-center sm:ml-44">
           <li className="flex w-full items-center text-blue-600 after:inline-block after:h-1 after:w-full after:border-4 after:border-b after:border-blue-100 after:content-[''] dark:text-blue-500 dark:after:border-blue-800">
             <span className="flex h-10 w-10 shrink-0 items-center justify-center rounded-full bg-blue-100 dark:bg-blue-800 lg:h-12 lg:w-12">
               <img className="h-[20px] w-[20px] " src={monster} />
@@ -253,11 +249,11 @@ const Screen3: React.FC<Screen3Props> = ({ onPrev, onNext }) => {
           &nbsp; 어떤 기능을 구현했나요?
         </div>
         {/* 기능 구현 입력창 */}
-        <div className="relative mt-8 h-80 border" data-te-input-wrapper-init>
+        <div className="relative mt-8 h-355" data-te-input-wrapper-init>
           <input
             type="text"
             maxLength={20}
-            className="ml-12 mb-7 flex w-10/12 justify-center border-b-2 focus:outline-none"
+            className="ml-9  mb-7 flex w-10/12 justify-center border-b-2 focus:outline-none sm:ml-12"
             placeholder=" 예시 - 구인글 작성 (최대 5개 작성 가능)"
           />
           {Arr.map((x, y) => (
@@ -265,13 +261,13 @@ const Screen3: React.FC<Screen3Props> = ({ onPrev, onNext }) => {
               key={y}
               type="text"
               maxLength={20}
-              className="ml-12 mb-7 flex w-10/12 justify-center border-b-2 focus:outline-none"
+              className=" ml-9  mb-7 flex w-10/12 justify-center border-b-2 focus:outline-none sm:ml-12"
               placeholder="기능을 입력해주세요"
             />
           ))}
           {Arr.length < 4 ? (
             <button
-              className="ml-[297px] mt-5 flex items-center justify-center"
+              className="ml-48 mt-5 flex items-center justify-center sm:ml-[297px]"
               onClick={() => Plus()}
             >
               <img src={plus} />
@@ -281,9 +277,9 @@ const Screen3: React.FC<Screen3Props> = ({ onPrev, onNext }) => {
         {/* 하단버튼 */}
         <button
           onClick={onNext}
-          className="fixed z-40 my-auto ml-14 mb-3
-          flex items-center justify-center
-        rounded-[16px] bg-gptbutton px-60 py-1 pt-3 pb-3 font-lef-b text-slate-50  hover:bg-button"
+          className="fixed z-40 my-auto mb-3
+          ml-7 flex items-center justify-center rounded-[16px]  bg-gptbutton px-40
+        py-1 pt-3 pb-3 font-lef-b text-slate-50 hover:bg-button sm:ml-504  sm:px-60"
         >
           다음
         </button>
@@ -312,96 +308,92 @@ const Screen4: React.FC<Screen4Props> = ({ onPrev }) => {
   return (
     <div
       className="-translate-y-1/2-translate-y-1/2 fixed top-20 right-1/2 z-50 box-border h-660
-        w-630 translate-x-1/2 transform 
-        rounded-[30px] bg-white"
+        w-[410px] translate-x-1/2 transform rounded-[30px] 
+        bg-white sm:w-630"
     >
       {/* 이전/다음 */}
-      <div className="mt-5">
-        <div className="ml-5 flex font-lef text-gray-400 ">
-          <button
-            onClick={onPrev}
-            className="flex w-56 items-center  hover:text-gray-600"
-          >
-            <img className="mr-2" src={arrowLeftIcon} />
-            이전
-          </button>
-        </div>
+      <button
+        onClick={onPrev}
+        className="mx-auto mt-5 ml-5 flex  items-center font-lef text-gray-400 hover:text-gray-600 "
+      >
+        <img className="mr-2" src={arrowLeftIcon} />
+        <div>이전</div>
+      </button>
 
-        {/* 4단계 */}
-        <ol className="ml-44 mt-4 mb-8 flex w-80 items-center justify-center">
-          <li className="flex w-full items-center text-blue-600 after:inline-block after:h-1 after:w-full after:border-4 after:border-b after:border-blue-100 after:content-[''] dark:text-blue-500 dark:after:border-blue-800">
-            <span className="flex h-10 w-10 shrink-0 items-center justify-center rounded-full bg-blue-100 dark:bg-blue-800 lg:h-12 lg:w-12">
-              <img className="h-[20px] w-[20px] " src={monster} />
-            </span>
-          </li>
-          <li className="flex w-full items-center text-blue-600 after:inline-block after:h-1 after:w-full after:border-4 after:border-b after:border-blue-100 after:content-[''] dark:text-blue-500 dark:after:border-blue-800">
-            <span className="flex h-10 w-10 shrink-0 items-center justify-center rounded-full bg-blue-100 dark:bg-blue-800 lg:h-12 lg:w-12">
-              <img className="h-[20px] w-[20px] " src={desktop} />
-            </span>
-          </li>
-          <li className="flex w-full items-center text-blue-600 after:inline-block after:h-1 after:w-full after:border-4 after:border-b after:border-blue-100 after:content-[''] dark:text-blue-500 dark:after:border-blue-800">
-            <span className="flex h-10 w-10 shrink-0 items-center justify-center rounded-full bg-blue-100 dark:bg-blue-800 lg:h-12 lg:w-12">
-              <img className="h-[20px] w-[20px] " src={pick} />
-            </span>
-          </li>
-          <li className="flex w-full items-center ">
-            <span className="flex h-10 w-10 shrink-0 items-center justify-center rounded-full bg-blue-100 dark:bg-blue-800 lg:h-12 lg:w-12">
-              <img className="h-[20px] w-[20px] " src={eyes} />
-            </span>
-          </li>
-        </ol>
+      {/* 4단계 */}
+      <ol className="ml-16  mt-4 mb-8 flex w-80 items-center justify-center sm:ml-44">
+        <li className="flex w-full items-center text-blue-600 after:inline-block after:h-1 after:w-full after:border-4 after:border-b after:border-blue-100 after:content-[''] dark:text-blue-500 dark:after:border-blue-800">
+          <span className="flex h-10 w-10 shrink-0 items-center justify-center rounded-full bg-blue-100 dark:bg-blue-800 lg:h-12 lg:w-12">
+            <img className="h-[20px] w-[20px] " src={monster} />
+          </span>
+        </li>
+        <li className="flex w-full items-center text-blue-600 after:inline-block after:h-1 after:w-full after:border-4 after:border-b after:border-blue-100 after:content-[''] dark:text-blue-500 dark:after:border-blue-800">
+          <span className="flex h-10 w-10 shrink-0 items-center justify-center rounded-full bg-blue-100 dark:bg-blue-800 lg:h-12 lg:w-12">
+            <img className="h-[20px] w-[20px] " src={desktop} />
+          </span>
+        </li>
+        <li className="flex w-full items-center text-blue-600 after:inline-block after:h-1 after:w-full after:border-4 after:border-b after:border-blue-100 after:content-[''] dark:text-blue-500 dark:after:border-blue-800">
+          <span className="flex h-10 w-10 shrink-0 items-center justify-center rounded-full bg-blue-100 dark:bg-blue-800 lg:h-12 lg:w-12">
+            <img className="h-[20px] w-[20px] " src={pick} />
+          </span>
+        </li>
+        <li className="flex w-full items-center ">
+          <span className="flex h-10 w-10 shrink-0 items-center justify-center rounded-full bg-blue-100 dark:bg-blue-800 lg:h-12 lg:w-12">
+            <img className="h-[20px] w-[20px] " src={eyes} />
+          </span>
+        </li>
+      </ol>
 
-        {/* screen4 */}
-        {/* 질문 */}
-        <div className="flex items-center justify-center font-lef-b text-[20px]">
-          <img className="h-[20px] w-[20px] " src={eyes} />
-          &nbsp; 관심있는 고도화 계획이 있나요?
-        </div>
-        {/* 고도화 기술 입력창 */}
-        <div className="relative mt-8" data-te-input-wrapper-init>
+      {/* screen4 */}
+      {/* 질문 */}
+      <div className="flex items-center justify-center font-lef-b text-[20px]">
+        <img className="h-[20px] w-[20px] " src={eyes} />
+        &nbsp; 관심있는 고도화 계획이 있나요?
+      </div>
+      {/* 고도화 기술 입력창 */}
+      <div className="relative mt-8 h-355" data-te-input-wrapper-init>
+        <input
+          type="text"
+          maxLength={20}
+          className="ml-9 mb-7 flex w-10/12 justify-center border-b-2 focus:outline-none sm:ml-12"
+          placeholder=" 예시 - 캐싱 (최대 5개 작성 가능)"
+        />{' '}
+        {Arr.map((x, y) => (
           <input
-            type="text"
+            key={y}
             maxLength={20}
-            className="ml-12 mb-7 flex w-10/12 justify-center border-b-2 focus:outline-none"
-            placeholder=" 예시 - 캐싱 (최대 5개 작성 가능)"
-          />{' '}
-          {Arr.map((x, y) => (
-            <input
-              key={y}
-              maxLength={20}
-              type="text"
-              className="ml-12 mb-7 flex w-10/12 justify-center border-b-2 focus:outline-none"
-              placeholder="고도화 계획을 입력해주세요"
-            />
-          ))}
-          {Arr.length < 4 ? (
-            <button
-              className=" ml-[297px] mt-5 flex h-5 w-5 items-center justify-center"
-              onClick={() => Plus()}
-            >
-              <img src={plus} />
-            </button>
-          ) : null}
-          {/* <img
+            type="text"
+            className="ml-9 mb-7 flex w-10/12 justify-center border-b-2 focus:outline-none sm:ml-12"
+            placeholder="고도화 계획을 입력해주세요"
+          />
+        ))}
+        {Arr.length < 4 ? (
+          <button
+            className="ml-48 mt-5 flex h-5 w-5 items-center justify-center sm:ml-[297px]"
+            onClick={() => Plus()}
+          >
+            <img src={plus} />
+          </button>
+        ) : null}
+        {/* <img
             className=" ml-[21rem] mt-5 flex h-5 w-5 items-center justify-center"
             src={plus}
           /> */}
-        </div>
-        {/* 하단버튼 */}
-        <button
-          className="fixed mt-20 ml-14
-          items-center justify-center
-        rounded-[16px] bg-gptbutton px-52 py-1 pt-3 pb-3 font-lef-b text-slate-50  hover:bg-button"
-        >
-          AI 고도화 추천
-        </button>
       </div>
+      {/* 하단버튼 */}
+      <button
+        className="fixed ml-7
+          items-center justify-center  rounded-[16px] bg-gptbutton
+        px-32 py-1 pt-3 pb-3 font-lef-b text-slate-50 hover:bg-button sm:ml-14  sm:px-52"
+      >
+        AI 고도화 추천
+      </button>
+
       {/* {children} */}
     </div>
   );
 };
 
-// 모달 컴포넌트
 interface ModalDefaultType {
   onClickToggleModal: () => void;
 }
