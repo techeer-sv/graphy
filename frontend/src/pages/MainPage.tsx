932ac11c2bef066a9bc9b405df4aa53caab3512c
Mayreeel <kuwon15@gmail.com>
2023-06-17 03:16:21 +0900

    feat : 무한스크롤 기능구현

--- frontend/src/pages/MainPage.tsx
+++ frontend/src/pages/MainPage.tsx
@@ -1,8 +1,8 @@
-import { act } from '@testing-library/react';
+import { useInfiniteQuery } from '@tanstack/react-query';
 import axios from 'axios';
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { useRecoilState } from 'recoil';
+import { useRecoilValue } from 'recoil';
 
 import WriteIcon from '../assets/image/pencil-square.svg';
 import Banner from '../components/Banner';
@@ -11,8 +11,7 @@ import ProjectCard from '../components/ProjectCard';
 import { searchTextState } from '../Recoil';
 
 function MainPage() {
-  const [data, setData] = useState<any[]>([]);
-  const [searchText, setSearchText] = useRecoilState(searchTextState);
+  const searchText = useRecoilValue(searchTextState);
 
   const navigate = useNavigate(); // react-router-dom useNavigate 사용 선언
 
@@ -21,31 +20,50 @@ function MainPage() {
     navigate('/write');
   }
 
-  async function getCard() {
-    const url = 'http://localhost:8080/api/v1/projects/search';
-    const params =
-      searchText === ''
-        ? {}
-        : {
-            projectName: searchText,
-          };
-    try {
-      const res = await axios.get(url, { params });
-      act(() => {
-        setData(res.data.data);
-      });
-    } catch (error) {
-      if (!navigator.onLine) {
-        alert('오프라인 상태입니다. 네트워크 연결을 확인해주세요.');
-        setSearchText('');
-      }
-      console.log(error);
-    }
+  async function getData({ pageParam = 1 }) {
+    const params = searchText
+      ? { projectName: searchText }
+      : { page: pageParam, size: 12 };
+    const res = await axios.get(
+      'http://localhost:8080/api/v1/projects/search',
+      {
+        params,
+      },
+    );
+    console.log(res.data);
+    return res.data.data;
   }
 
+  const { data, fetchNextPage, hasNextPage, isFetchingNextPage, status } =
+    useInfiniteQuery(['projects'], getData, {
+      getNextPageParam: (lastPage, pages) =>
+        lastPage.length < 12 ? undefined : pages.length + 1,
+    });
+
+  // Trigger the next page load when the user scrolls to the bottom of the page.
   useEffect(() => {
-    getCard(); // 랜더링이 될 때 실행되는 함수
-  }, [searchText]); // 변수가 들어가있으면 변수가 바뀔 때마다 useEffect 안에 있는 함수를 실행시킴
+    if (!isFetchingNextPage) {
+      const handleScroll = () => {
+        if (
+          document.documentElement.scrollHeight -
+            document.documentElement.scrollTop ===
+          document.documentElement.clientHeight
+        ) {
+          // eslint-disable-next-line consistent-return
+          fetchNextPage();
+        }
+      };
+      window.addEventListener('scroll', handleScroll);
+    }
+  }, [fetchNextPage, isFetchingNextPage]);
+
+  if (status === 'loading') {
+    return <span>Loading...</span>;
+  }
+
+  if (status === 'error') {
+    return <span>Error fetching data</span>;
+  }
 
   return (
     <div className="relative h-auto min-h-screen w-screen bg-gray-50">
@@ -70,30 +88,38 @@ function MainPage() {
           {/* All */}
         </div>
 
-        {/* 프로젝트 카드 리스트 */}
-        <div className="">
+        <div>
           {searchText === '' ? (
-            <div className="relative mx-8 flex flex-wrap justify-center pt-6 sm:pt-8">
-              {' '}
-              {data.map((item) => (
-                <div className="mx-8 mb-10" key={item.id}>
-                  <ProjectCard items={item} />
+            <>
+              {data.pages.map((group, i) => (
+                <div
+                  className="relative mx-8 flex flex-wrap justify-center pt-6 sm:pt-8"
+                  key={group[i]?.id}
+                >
+                  {group.map((item: any) => (
+                    <div className="mx-8 mb-10" key={item.id}>
+                      <ProjectCard items={item} />
+                    </div>
+                  ))}
                 </div>
-              ))}{' '}
-            </div>
+              ))}
+              {hasNextPage && isFetchingNextPage && (
+                <span>Loading more...</span>
+              )}
+            </>
           ) : (
             <div className="ml-0 flex flex-wrap justify-center min-[680px]:ml-10 min-[680px]:justify-start">
-              {' '}
-              {data
+              {data.pages
+                .flat()
                 .filter((x) => x.projectName.includes(searchText))
                 .map((item) => (
                   <div
-                    className="mx-3 mt-9 min-[680px]:mx-0 min-[680px]:ml-16 "
+                    className="mx-3 mt-9 min-[680px]:mx-0 min-[680px]:ml-16"
                     key={item.id}
                   >
                     <ProjectCard items={item} />
                   </div>
-                ))}{' '}
+                ))}
             </div>
           )}
         </div>
