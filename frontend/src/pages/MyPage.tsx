7bc6b90ad856af019c97dfd2a97d580135c2101b
“mineii” <kmaoo0406@naver.com>
2023-06-18 17:35:48 +0900

    design:  Lato font

--- frontend/src/pages/MyPage.tsx
+++ frontend/src/pages/MyPage.tsx
@@ -1,13 +1,16 @@
-import NavBar from '../components/NavBar';
+// import NavBar from '../components/NavBar';
 // import ProjectCard from '../components/ProjectCard';
 
 function MyPage() {
   return (
     <div className="relative h-auto min-h-screen w-screen bg-graphybg">
-      <NavBar />
-      <div className="flex">
-        <div className="h-48 w-64 rounded-[25px] bg-white">닉네임</div>
-        <div className="text-[21px]">나의 작성 포스트</div>
+      {/* <NavBar /> */}
+      <div className="inline-flex">
+        <div className="h-48 w-64 rounded-[25px] bg-white font-lato">
+          닉네임
+        </div>
+        <div className="font-lato text-[21px]">나의 작성 포스트</div>
+        <div className="mx-10 border-b-2 border-b-neutral-300 pt-0 font-ng-b text-2xl sm:mx-28 sm:mb-5 sm:pt-5" />
       </div>
     </div>
   );
