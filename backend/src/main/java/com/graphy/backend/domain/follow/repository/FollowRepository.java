7051b85acba32ff68f88af1ed4d53d527132266b
dbrjs4594283 <dbrjs4594283@naver.com>
2023-06-23 01:45:28 +0900

    feat:팔로우 도메인 API(등록, 조회, 삭제) 구현

---
+++ backend/src/main/java/com/graphy/backend/domain/follow/repository/FollowRepository.java
@@ -0,0 +1,25 @@
+package com.graphy.backend.domain.follow.repository;
+
+import com.graphy.backend.domain.follow.domain.Follow;
+import com.graphy.backend.domain.follow.dto.FollowListDto;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface FollowRepository extends JpaRepository<Follow, Long> {
+    boolean existsByFromIdAndToId(Long fromId, Long toId);
+    Optional<Follow> findByFromIdAndToId(Long fromId, Long toId);
+    @Query(value = "select m.id, m.nickname " +
+            "from Follow f " +
+            "inner join Member m on m.id = f.from_id " +
+            "where f.to_id = :toId", nativeQuery = true)
+    List<FollowListDto> findFollower(Long toId);
+
+    @Query(value = "select m.id, m.nickname " +
+            "from Follow f " +
+            "inner join Member m on m.id = f.to_id " +
+            "where f.from_id = :toId", nativeQuery = true)
+    List<FollowListDto> findFollowing(Long toId);
+}
