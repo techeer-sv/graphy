f38aca6ad7bbfa47f3940526c1d542c37b39e353
dbrjs4594283 <dbrjs4594283@naver.com>
2023-06-23 13:45:06 +0900

    test:follow 도메인 서비스 계층 테스트 코드 작성

--- backend/src/test/java/com/graphy/backend/domain/follow/service/FollowServiceTest.java
+++ backend/src/test/java/com/graphy/backend/domain/follow/service/FollowServiceTest.java
@@ -1,9 +1,181 @@
 package com.graphy.backend.domain.follow.service;
 
+import com.graphy.backend.domain.follow.domain.Follow;
+import com.graphy.backend.domain.follow.dto.FollowListDto;
+import com.graphy.backend.domain.follow.repository.FollowRepository;
+import com.graphy.backend.domain.member.domain.Member;
+import com.graphy.backend.global.auth.jwt.CustomUserDetailsService;
+import com.graphy.backend.global.error.exception.AlreadyFollowingException;
+import com.graphy.backend.global.error.exception.EmptyResultException;
 import com.graphy.backend.test.MockTest;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.util.ReflectionTestUtils;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 public class FollowServiceTest extends MockTest {
+    @Mock
+    FollowRepository followRepository;
+    @Mock
+    CustomUserDetailsService customUserDetailsService;
+    @InjectMocks
+    FollowService followService;
+
+    @Test
+    @DisplayName("팔로우 신청 테스트")
+    public void followTest() throws Exception {
+        //given
+        Member fromMember = Member.builder().id(1L).build();
+        Long toId = 2L;
+
+        //when
+        when(customUserDetailsService.getLoginUser()).thenReturn(fromMember);
+        followService.follow(toId);
+
+        //then
+        verify(followRepository, times(1)).save(any(Follow.class));
+        verify(customUserDetailsService, times(1)).getLoginUser();
+    }
+
+    @Test
+    @DisplayName("팔로잉 리스트 조회 테스트")
+    public void getFollowingListTest() throws Exception {
+        //given
+        Member fromMember = Member.builder().id(1L).build();
+        FollowListDto following1 = new FollowListDto() {
+            public Long getId() {
+                return 2L;
+            }
+            public String getNickname() {
+                return "memberA";
+            }
+        };
+
+        FollowListDto following2 = new FollowListDto() {
+            public Long getId() {
+                return 3L;
+            }
+            public String getNickname() {
+                return "memberB";
+            }
+        };
+        List<FollowListDto> followingList = Arrays.asList(following1, following2);
+
+        //when
+        when(customUserDetailsService.getLoginUser()).thenReturn(fromMember);
+        when(followRepository.findFollowing(fromMember.getId())).thenReturn(followingList);
+        List<FollowListDto> result = followService.getFollowings();
+
+        //then
+        Assertions.assertThat(result.get(0).getId()).isEqualTo(2L);
+        Assertions.assertThat(result.get(0).getNickname()).isEqualTo("memberA");
+        Assertions.assertThat(result.get(1).getId()).isEqualTo(3L);
+        Assertions.assertThat(result.get(1).getNickname()).isEqualTo("memberB");
+    }
+
+    @Test
+    @DisplayName("팔로워 리스트 조회 테스트")
+    public void getFollowerListTest() throws Exception {
+        //given
+        Member toMember = Member.builder().id(1L).build();
+        FollowListDto follower1 = new FollowListDto() {
+            public Long getId() {
+                return 2L;
+            }
+            public String getNickname() {
+                return "memberA";
+            }
+        };
+
+        FollowListDto follower2 = new FollowListDto() {
+            public Long getId() {
+                return 3L;
+            }
+            public String getNickname() {
+                return "memberB";
+            }
+        };
+        List<FollowListDto> followerList = Arrays.asList(follower1, follower2);
+
+        //when
+        when(customUserDetailsService.getLoginUser()).thenReturn(toMember);
+        when(followRepository.findFollower(toMember.getId())).thenReturn(followerList);
+        List<FollowListDto> result = followService.getFollowers();
+
+        //then
+        Assertions.assertThat(result.get(0).getId()).isEqualTo(2L);
+        Assertions.assertThat(result.get(0).getNickname()).isEqualTo("memberA");
+        Assertions.assertThat(result.get(1).getId()).isEqualTo(3L);
+        Assertions.assertThat(result.get(1).getNickname()).isEqualTo("memberB");
+    }
+
+    @Test
+    @DisplayName("언팔로우 테스트")
+    public void unfollowTest() throws Exception {
+        //given
+        Long toId = 1L;
+        Member fromMember = Member.builder().id(2L).build();
+        Follow follow = Follow.builder().fromId(fromMember.getId()).toId(toId).build();
+
+        //when
+        when(customUserDetailsService.getLoginUser()).thenReturn(fromMember);
+        when(followRepository.findByFromIdAndToId(fromMember.getId(), toId))
+                .thenReturn(Optional.ofNullable(follow));
+        followService.unfollow(toId);
+
+        //then
+        verify(followRepository, times(1)).delete(follow);
+    }
+
+    @Test
+    @DisplayName("존재하지 않는 팔로우 예외처리 테스트")
+    public void unfollowNotFoundTest() {
+        // given
+        Long toId = 1L;
+        Member fromMember = Member.builder().id(2L).build();
+
+        when(customUserDetailsService.getLoginUser()).thenReturn(fromMember);
+        when(followRepository.findByFromIdAndToId(fromMember.getId(), toId))
+                .thenReturn(Optional.empty());
+
+        // when
+        Exception exception = assertThrows(EmptyResultException.class, () -> {
+            followService.unfollow(toId);
+        });
+
+        // then
+        String exceptionMessage = exception.getMessage();
+
+        assertTrue(exceptionMessage.equals("존재하지 않는 팔로우"));
+    }
+
+    @Test
+    @DisplayName("팔로우 여부 체크 테스트")
+    public void followingCheckTest() throws Exception {
+        //when
+        when(followRepository.existsByFromIdAndToId(1L, 2L)).thenReturn(true);
+        when(followRepository.existsByFromIdAndToId(3L, 4L)).thenReturn(false);
+
+        // then
+        assertThrows(AlreadyFollowingException.class, () -> {
+            ReflectionTestUtils.invokeMethod(followService, "followingCheck", 1L, 2L);
+        });
+
+        assertDoesNotThrow(() -> {
+            ReflectionTestUtils.invokeMethod(followService, "followingCheck", 3L, 4L);
+        });
+
+    }
 }
