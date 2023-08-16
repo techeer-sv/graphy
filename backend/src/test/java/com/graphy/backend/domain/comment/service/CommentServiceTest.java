1ad3d5d7a332d6f715b68c9b4fcbe5dd1c324fb2
youKeon <yukeon97@gmail.com>
2023-08-16 15:09:32 +0900

    test : Comment 도메인 서비스 계층 테스트 코드 작성

--- backend/src/test/java/com/graphy/backend/domain/comment/service/CommentServiceTest.java
+++ backend/src/test/java/com/graphy/backend/domain/comment/service/CommentServiceTest.java
@@ -1,24 +1,27 @@
 package com.graphy.backend.domain.comment.service;
 
 import com.graphy.backend.domain.comment.domain.Comment;
+import com.graphy.backend.domain.comment.dto.request.CreateCommentRequest;
+import com.graphy.backend.domain.comment.dto.request.UpdateCommentRequest;
+import com.graphy.backend.domain.comment.dto.response.GetReplyListResponse;
 import com.graphy.backend.domain.comment.repository.CommentRepository;
 import com.graphy.backend.domain.member.domain.Member;
+import com.graphy.backend.domain.member.domain.Role;
 import com.graphy.backend.domain.project.domain.Project;
 import com.graphy.backend.domain.project.repository.ProjectRepository;
-import com.graphy.backend.global.auth.jwt.CustomUserDetailsService;
+import com.graphy.backend.global.error.exception.EmptyResultException;
 import com.graphy.backend.test.MockTest;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.dao.EmptyResultDataAccessException;
+import java.util.*;
 
-import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.Optional;
-
+import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.BDDMockito.*;
 
@@ -32,64 +35,199 @@ class CommentServiceTest extends MockTest {
     private CommentRepository commentRepository;
     @Mock
     private ProjectRepository projectRepository;
-    @Mock
-    private CustomUserDetailsService customUserDetailsService;
+
+    private Member member;
+    private Project project;
+    private Comment parentComment;
+    @BeforeEach
+    public void setup() {
+        member = Member.builder()
+                .id(1L)
+                .email("graphy@gmail.com")
+                .nickname("name")
+                .role(Role.ROLE_USER)
+                .build();
+
+        project = Project.builder()
+                .id(1L)
+                .member(member)
+                .build();
+
+        parentComment = Comment.builder()
+                .id(1L)
+                .content("parentComment")
+                .member(member)
+                .project(project)
+                .build();
+
+    }
 
     @Test
     @DisplayName("댓글이 생성된다")
     void createCommentTest() {
-
         //given
-
-        // mocking
+        CreateCommentRequest request = new CreateCommentRequest("content", project.getId(), null);
 
         //when
+        when(projectRepository.findById(project.getId())).thenReturn(Optional.ofNullable(project));
 
         //then
+        assertDoesNotThrow(() -> commentService.addComment(request, member));
     }
 
     @Test
-    @DisplayName("대댓글이 생성된다")
-    void createCommentTest2() {
+    @DisplayName("댓글 생성 시 프로젝트가 존재하지 않으면 예외가 발생한다")
+    void createCommentNotExistProjectExceptionTest() {
+        // given
+        Long 존재하지_않는_프로젝트_ID = 0L;
+        CreateCommentRequest request = new CreateCommentRequest("content", 존재하지_않는_프로젝트_ID, null);
 
-        //given
+        // when, then
+        assertThatThrownBy(
+                () -> commentService.addComment(request, member))
+                .isInstanceOf(EmptyResultException.class)
+                .hasMessageContaining("이미 삭제되거나 존재하지 않는 프로젝트");
     }
 
     @Test
-    @DisplayName("댓글과 답글 상관없이 삭제")
-    public void deleteComment() throws Exception {
+    @DisplayName("답글이 생성된다")
+    void addReCommentTest() {
         //given
-        Comment comment = Comment.builder().id(1L).content("댓글").build();
-        Comment reComment = Comment.builder().id(2L).content("대댓글").parent(comment).build();
+        CreateCommentRequest request = new CreateCommentRequest("content", project.getId(), parentComment.getId());
 
-        given(commentRepository.findById(1L)).willReturn(Optional.of(comment));
-        given(commentRepository.findById(2L)).willReturn(Optional.of(reComment));
+        //when
+        when(projectRepository.findById(request.getProjectId())).thenReturn(Optional.ofNullable(project));
+        when(commentRepository.findById(request.getParentId())).thenReturn(Optional.ofNullable(parentComment));
+
+        //then
+        assertDoesNotThrow(() -> commentService.addComment(request, member));
+    }
+
+    @Test
+    @DisplayName("답글 생성 시 댓글이 존재하지 않으면 예외가 발생한다")
+    void addReCommentNotExistParentCommentExceptionTest() {
+        // given
+        Long 존재하지_않는_댓글_ID = 0L;
+        CreateCommentRequest request = new CreateCommentRequest("content", project.getId(), 존재하지_않는_댓글_ID);
 
         // when
-        commentService.deleteComment(1L);
-        commentService.deleteComment(2L);
+        when(projectRepository.findById(request.getProjectId())).thenReturn(Optional.ofNullable(project));
+
+        // then
+        assertThatThrownBy(
+                () -> commentService.addComment(request, member))
+                .isInstanceOf(EmptyResultException.class)
+                .hasMessageContaining("이미 삭제되거나 존재하지 않는 댓글");
+    }
+
+    @Test
+    @DisplayName("댓글을 수정한다")
+    void modifyCommentTest() {
+        //given
+        UpdateCommentRequest request = new UpdateCommentRequest("updateContent");
+
+        //when
+        when(commentRepository.findById(parentComment.getId())).thenReturn(Optional.ofNullable(parentComment));
+        commentService.modifyComment(parentComment.getId(), request);
 
         //then
-        assertTrue(comment.isDeleted());
-        assertTrue(reComment.isDeleted());
+        assertThat(parentComment.getContent()).isEqualTo(request.getContent());
     }
 
     @Test
-    @DisplayName("댓글 수정")
-    public void updateComment() throws Exception {
+    @DisplayName("답글 수정 시 댓글이 존재하지 않으면 예외가 발생한다")
+    void modifyReCommentNotExistParentCommentExceptionTest() {
+        // given
+        Long 존재하지_않는_댓글_ID = 0L;
+        UpdateCommentRequest request = new UpdateCommentRequest("updateContent");
+
+        // when, then
+        assertThatThrownBy(
+                () -> commentService.modifyComment(존재하지_않는_댓글_ID, request))
+                .isInstanceOf(EmptyResultException.class)
+                .hasMessageContaining("이미 삭제되거나 존재하지 않는 댓글");
+    }
+
+    @Test
+    @DisplayName("댓글을 삭제한다")
+    void removeCommentTest() {
+        //when
+        when(commentRepository.findById(parentComment.getId())).thenReturn(Optional.ofNullable(parentComment));
+        commentService.deleteComment(parentComment.getId());
+
+        //then
+        assertThat(parentComment.isDeleted()).isTrue();
+    }
+
+    @Test
+    @DisplayName("답글 삭제 시 댓글이 존재하지 않으면 예외가 발생한다")
+    void removeReCommentNotExistParentCommentExceptionTest() {
+        // given
+        Long 존재하지_않는_댓글_ID = 0L;
+
+        // when, then
+        assertThatThrownBy(
+                () -> commentService.deleteComment(존재하지_않는_댓글_ID))
+                .isInstanceOf(EmptyResultException.class)
+                .hasMessageContaining("이미 삭제되거나 존재하지 않는 댓글");
+    }
+
+    @Test
+    @DisplayName("답글 목록을 조회한다")
+    void findReCommentListTest() {
         //given
+        Comment reComment1 = Comment.builder()
+                .id(2L)
+                .content("reComment1")
+                .parent(parentComment)
+                .member(member)
+                .build();
+
+        Comment reComment2 = Comment.builder()
+                .id(3L)
+                .content("reComment2")
+                .parent(parentComment).member(member)
+                .build();
+
+        Comment reComment3 = Comment.builder()
+                .id(4L)
+                .content("reComment3")
+                .parent(parentComment)
+                .member(member)
+                .build();
+
+        List<Comment> commentList = new LinkedList<>(List.of(reComment1, reComment2, reComment3));
+
+        //when
+        when(commentRepository.findReplyList(parentComment.getId())).thenReturn(commentList);
+        List<GetReplyListResponse> actual = commentService.findCommentList(parentComment.getId());
+
+        //then
+        assertThat(actual.size()).isEqualTo(commentList.size());
+
+        assertThat(actual.get(0).getCommentId()).isEqualTo(reComment1.getId());
+        assertThat(actual.get(0).getContent()).isEqualTo(reComment1.getContent());
+
+        assertThat(actual.get(1).getCommentId()).isEqualTo(reComment2.getId());
+        assertThat(actual.get(1).getContent()).isEqualTo(reComment2.getContent());
+
+        assertThat(actual.get(2).getCommentId()).isEqualTo(reComment3.getId());
+        assertThat(actual.get(2).getContent()).isEqualTo(reComment3.getContent());
     }
 
     @Test
-    @DisplayName("답글 조회")
-    public void getReplyList() throws Exception {
+    @DisplayName("답글 목록 조회 시 목록이 없으면 예외가 발생한다")
+    void findReCommentListEmptyListExceptionTest() {
+        // given
+        Long 답글이_존재하지_않는_댓글_ID = 0L;
 
-    }
+        //when
+        when(commentRepository.findReplyList(답글이_존재하지_않는_댓글_ID)).thenReturn(Collections.emptyList());
 
-    @Test
-    @DisplayName("삭제되거나 없는 댓글 수정시 오류가 발생한다")
-    void updateCommentTest() {
-
-        //given
+        // then
+        assertThatThrownBy(
+                () -> commentService.findCommentList(답글이_존재하지_않는_댓글_ID))
+                .isInstanceOf(EmptyResultException.class)
+                .hasMessageContaining("이미 삭제되거나 존재하지 않는 댓글");
     }
 }
