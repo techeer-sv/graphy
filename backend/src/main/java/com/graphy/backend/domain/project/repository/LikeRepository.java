2d0a7f4a59a3ce4f6d8fb5df8868c418bb1bbd8e
halin <halin130@naver.com>
2023-09-02 12:13:10 +0900

    refactor: LikedMember 조회 쿼리 방식 QueryDSL로 변경

--- backend/src/main/java/com/graphy/backend/domain/project/repository/LikeRepository.java
+++ backend/src/main/java/com/graphy/backend/domain/project/repository/LikeRepository.java
@@ -1,25 +1,16 @@
 package com.graphy.backend.domain.project.repository;
 
-import com.graphy.backend.domain.member.dto.response.GetMemberListResponse;
 import com.graphy.backend.domain.member.domain.Member;
 import com.graphy.backend.domain.project.domain.Like;
 import com.graphy.backend.domain.project.domain.Project;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 import java.util.Optional;
 
 @Repository
-public interface LikeRepository extends JpaRepository<Like, Long> {
+public interface LikeRepository extends JpaRepository<Like, Long>, LikeCustomRepository {
 
     Optional<Like> findByProjectAndMember(Project project, Member member);
-
-    @Query(value = "select m.id, m.nickname " +
-            "from Like l " +
-            "inner join Member m on m.id = l.member_id " +
-            "where l.project_id = :projectId", nativeQuery = true)
-    List<GetMemberListResponse> findLikedMembers(Long projectId);
 }
 
