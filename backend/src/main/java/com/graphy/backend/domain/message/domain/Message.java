f76bb70978599bcfaa7ff54277445c28fcafc5ed
youKeon <yukeon97@gmail.com>
2023-09-27 00:46:09 +0900

    refactor : 엔티티 세팅 변경

---
+++ backend/src/main/java/com/graphy/backend/domain/message/domain/Message.java
@@ -0,0 +1,28 @@
+package com.graphy.backend.domain.message.domain;
+
+import com.graphy.backend.domain.member.domain.Member;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Message {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "sender_id")
+    private Member sender;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "receiver_id")
+    private Member receiver;
+}
