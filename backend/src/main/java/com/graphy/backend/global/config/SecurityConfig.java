4576543e00eac3eb2e4811486cd739961f28ea7a
hangyeol <snake011128@naver.com>
2023-09-29 01:24:17 +0900

    fix: findList API 변경으로 인한 SecurityConfig 변경

--- backend/src/main/java/com/graphy/backend/global/config/SecurityConfig.java
+++ backend/src/main/java/com/graphy/backend/global/config/SecurityConfig.java
@@ -38,7 +38,7 @@ public class SecurityConfig {
                 .antMatchers("/api/v1/auth/signup",
                         "/api/v1/auth/signin",
                         "/api/v1/auth/logout",
-                        "/api/v1/projects/search",
+                        "/api/v1/projects", 
                         "/swagger-ui/**").permitAll()
                 .antMatchers(HttpMethod.GET, "/api/v1/projects/{projectId}").permitAll()
                 .antMatchers(HttpMethod.GET, "/api/v1/comments/{commentId}").permitAll()
