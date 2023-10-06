8e4fc8a2f66b085bd89d961bd18a97f2db94c1a9
Mayreeel <kuwon15@gmail.com>
2023-10-06 15:35:57 +0900

    feat : NavBar 마이그레이션
    Layout.tsx를 Wrapping하는 방식으로 구현
    PathName으로 예외처리 및 useEffect 의존성 배열 변수 할당

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -2,6 +2,7 @@ import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import Provider from '../components/general/Provider'
+import NavBar from '../components/general/NavBar'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -18,7 +19,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Provider>{children}</Provider>
+        <Provider>
+          <NavBar>{children}</NavBar>
+        </Provider>
       </body>
     </html>
   )
