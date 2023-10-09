f3c591c89f4501044a4c8f24b9bfc1a4b5c59eae
hangyeol <snake011128@naver.com>
2023-10-10 02:06:31 +0900

    fix: Job 테스트 코드 삭제

--- backend/src/test/java/com/graphy/backend/domain/job/controller/JobControllerTest.java
+++ backend/src/test/java/com/graphy/backend/domain/job/controller/JobControllerTest.java
@@ -4,8 +4,6 @@ package com.graphy.backend.domain.job.controller;
 import com.graphy.backend.domain.job.service.JobService;
 import com.graphy.backend.test.MockApiTest;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -14,13 +12,6 @@ import org.springframework.restdocs.RestDocumentationContextProvider;
 import org.springframework.restdocs.RestDocumentationExtension;
 import org.springframework.web.context.WebApplicationContext;
 
-import static org.mockito.Mockito.*;
-import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
-import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.post;
-import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessResponse;
-import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(JobController.class)
 @ExtendWith(RestDocumentationExtension.class)
@@ -33,30 +24,5 @@ class JobControllerTest extends MockApiTest {
     @BeforeEach
     public void setup(RestDocumentationContextProvider provider) {
         this.mvc = buildMockMvc(context, provider);
-    }
-
-
-    @Test
-    @DisplayName("공고를 저장한다.")
-    void saveJobInfo() throws Exception {
-        doNothing().when(jobService).save();
-        mvc.perform(post("/api/v1/jobs"))
-                .andExpect(status().isOk())
-                .andDo(document("JobInfo-Save",
-                        preprocessResponse(prettyPrint()))
-                );
-        verify(jobService, times(1)).save();
-    }
-
-    @Test
-    @DisplayName("공고를 삭제한다.")
-    public void deleteJobInfo() throws Exception {
-        doNothing().when(jobService).deleteExpiredJobs();
-        mvc.perform(delete("/api/v1/jobs"))
-                .andExpect(status().isOk())
-                .andDo(document("JobInfo-Delete",
-                        preprocessResponse(prettyPrint()))
-                );
-        verify(jobService, times(1)).deleteExpiredJobs();
     }
 }
