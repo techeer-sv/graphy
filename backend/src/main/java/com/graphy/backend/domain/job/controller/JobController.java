f9226155a1abcf53158a64b8beadc8f2ad4b2444
hangyeol <snake011128@naver.com>
2023-10-10 01:46:34 +0900

    fix: 안쓰는 코드 삭제

--- backend/src/main/java/com/graphy/backend/domain/job/controller/JobController.java
+++ backend/src/main/java/com/graphy/backend/domain/job/controller/JobController.java
@@ -14,7 +14,6 @@ import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.web.bind.annotation.*;
 
 @Tag(name = "EmploymentController", description = "신규 채용 공고 API")
@@ -23,21 +22,6 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor(access = AccessLevel.PROTECTED)
 public class JobController {
     private final JobService jobService;
-
-    @Operation(summary = "Save JobInfo", description = "신규 공고 저장")
-    @Scheduled(cron = "0 0 0 */3 * *")
-    @PostMapping
-    public void save(){
-        jobService.save();
-    }
-
-
-    @Operation(summary = "Delete Job", description = "만료일이 지난 공고 삭제")
-    @Scheduled(cron = "0 0 0 * * *")
-    @DeleteMapping
-    public void deleteExpiredJobs(){
-        jobService.deleteExpiredJobs();
-    }
 
     @Operation(summary = "findJobList", description = "채용공고 조회")
     @GetMapping
