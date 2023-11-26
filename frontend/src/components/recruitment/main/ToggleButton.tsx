441fa31cb694717ad21e7b2b75204b577d7c5166
rachel4w2 <rachel4w2@naver.com>
2023-11-27 01:57:45 +0900

    fix : 토글 버튼 반대로 작동하는 현상 수정

--- frontend/src/components/recruitment/main/ToggleButton.tsx
+++ frontend/src/components/recruitment/main/ToggleButton.tsx
@@ -1,26 +1,17 @@
-import { useEffect, useState } from 'react'
 import { useRecoilState } from 'recoil'
 import { recruitfilterState } from '../../../utils/atoms'
 import '../../../../public/css/toggle-button.css'
 
 export default function ToggleButton() {
   const [recruitFilter, setRecruitFilter] = useRecoilState(recruitfilterState)
-  const [recruiting, setRecruiting] = useState<boolean>(false)
 
   const handleToggle = () => {
-    setRecruiting(!recruiting)
     setRecruitFilter(!recruitFilter)
   }
 
-  useEffect(() => {
-    // if (recruitFilter === true) {
-    //   handleToggle()
-    // }
-  }, [])
-
   return (
     <label className="switch">
-      <input type="checkbox" checked={recruiting} onChange={handleToggle} />
+      <input type="checkbox" checked={recruitFilter} onChange={handleToggle} />
       <span className="slider round"> </span>
     </label>
   )
