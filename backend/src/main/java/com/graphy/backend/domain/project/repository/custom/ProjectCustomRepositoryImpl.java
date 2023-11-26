82f2768d822021afd7c538b251b039d1e6c17423
hangyeol <snake011128@naver.com>
2023-11-27 01:46:45 +0900

    fix: project와 recruitment 리스트 조회 repository에 정렬가능하게 구현

--- backend/src/main/java/com/graphy/backend/domain/project/repository/custom/ProjectCustomRepositoryImpl.java
+++ backend/src/main/java/com/graphy/backend/domain/project/repository/custom/ProjectCustomRepositoryImpl.java
@@ -2,6 +2,8 @@ package com.graphy.backend.domain.project.repository.custom;
 
 import com.graphy.backend.domain.project.domain.Project;
 import com.graphy.backend.domain.project.repository.ProjectCustomRepository;
+import com.graphy.backend.global.util.QueryDslUtil;
+import com.querydsl.core.types.OrderSpecifier;
 import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.JPQLQuery;
 import com.querydsl.jpa.impl.JPAQuery;
@@ -25,10 +27,12 @@ public class ProjectCustomRepositoryImpl implements ProjectCustomRepository {
 
     @Override
     public Page<Project> searchProjectsWith(Pageable pageable, String projectName, String content) {
+        List<OrderSpecifier> ORDERS = QueryDslUtil.getAllOrderSpecifiers(pageable, project.getMetadata().getName());
         List<Project> fetch = jpaQueryFactory
                 .selectFrom(project).where(projectNameLike(projectName), contentLike(content))
                 .offset(pageable.getOffset())
                 .limit(pageable.getPageSize())
+                .orderBy(ORDERS.stream().toArray(OrderSpecifier[]::new))
                 .fetch();
 
         JPQLQuery<Project> count = jpaQueryFactory
